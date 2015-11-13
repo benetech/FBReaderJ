73076975a48c8f92d6b76ee94c4b70238f20d118
Nima A <nimaa@benetech.org>
2015-11-13 11:16:23 -0500

    Refactor cleanup indentation

--- FBReader/src/main/java/org/accessibility/BenetechAccessibilityService.java
+++ FBReader/src/main/java/org/accessibility/BenetechAccessibilityService.java
@@ -14,15 +14,15 @@ public class BenetechAccessibilityService extends AccessibilityService
 	private static boolean isInitialized = false;
 	private static ArrayList<String> hold = new ArrayList<String>();
 	private static String information;
-		
-	public static void setActivity(Activity owner) 
+
+	public static void setActivity(Activity owner)
 	{
 		tts = new TextToSpeech(owner, new OnInitListener()
 		{
-			public void onInit(int status)  
+			public void onInit(int status)
 			{
 				System.out.println("initialize speech");
-				if (status == TextToSpeech.SUCCESS) 
+				if (status == TextToSpeech.SUCCESS)
 				{
 					isInitialized = true;
 					noConversionSpeak("Ready");
@@ -30,27 +30,27 @@ public class BenetechAccessibilityService extends AccessibilityService
 				}
 			}
 		});
-		}
-	
+	}
+
 	private static void checkHeld()
 	{
 		if (hold.isEmpty()) return;
-			for (String speak:hold)
-				process(speak);
-			hold.clear();
-}
-	
+		for (String speak:hold)
+			process(speak);
+		hold.clear();
+	}
+
 	@Override
-	public void onAccessibilityEvent(AccessibilityEvent e) 
+	public void onAccessibilityEvent(AccessibilityEvent e)
 	{
-		
+
 	}
 
 	public static void onReceiveAccessibilityEvent(Object message)
 	{
 		process(message);
-}
-	
+	}
+
 	private static void process(Object message)
 	{
 		if (message.getClass().equals(String.class)) information = (String)message;
@@ -58,38 +58,38 @@ public class BenetechAccessibilityService extends AccessibilityService
 		else information = message.toString();
 		if (isReady()) speak();
 	}
-	
+
 	private static boolean isReady()
 	{
-			if ((tts == null) || (!isInitialized))
-			{
-				if (!hold.contains(new String(information.toCharArray()))) 
-hold.add(new String(information.toCharArray()));
-				return false;
-			}
-			return true;
+		if ((tts == null) || (!isInitialized))
+		{
+			if (!hold.contains(new String(information.toCharArray())))
+				hold.add(new String(information.toCharArray()));
+			return false;
 		}
-	
-	@Override
-	public void onInterrupt()
-	{
-		
+		return true;
 	}
 
 	@Override
-	protected void onServiceConnected() 
+	public void onInterrupt()
+	{
+
+	}
+
+	@Override
+	protected void onServiceConnected()
 	{
 		super.onServiceConnected();
 	}
-	
+
 	private static void noConversionSpeak(String speak)
 	{
 		tts.speak(speak, TextToSpeech.QUEUE_ADD, null);
 	}
 
-public static void speak()
-{
-	noConversionSpeak(information);
-}
+	public static void speak()
+	{
+		noConversionSpeak(information);
+	}
 
 }
