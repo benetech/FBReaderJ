3f2985a8df23e45e6e40e5cb9d3889f98ea72692
Nima A <nimaa@benetech.org>
2015-11-24 14:05:38 -0500

    FBR-398: Removed background color in code,  needs to come from theme

--- FBReader/src/main/java/org/geometerplus/zlibrary/ui/android/library/ZLAndroidActivityWithNavigationDrawer.java
+++ FBReader/src/main/java/org/geometerplus/zlibrary/ui/android/library/ZLAndroidActivityWithNavigationDrawer.java
@@ -4,8 +4,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Configuration;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -57,7 +55,6 @@ public class ZLAndroidActivityWithNavigationDrawer extends AppCompatActivity imp
 		mDrawerLayout.setDrawerListener(mDrawerToggle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.navigation_bar_color)));
     }
 
     @Override
