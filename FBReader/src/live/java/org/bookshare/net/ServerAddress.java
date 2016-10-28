7e9e576562970f92fbb2eed5fe429cd9847b168f
Marco Sanchez <marco.avantica@avantica.net>
2016-10-28 14:27:52 -0600

    update gradle to 2.2.2 and adding to flavors for alpha and live changing the Host_name variable

---
+++ FBReader/src/live/java/org/bookshare/net/ServerAddress.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2016 Intellect Solution, Inc. All Rights Reserved
+ * http://i-solutionsinc.com
+ * Developed by: Avantica Technologies.
+ */
+package org.bookshare.net;
+
+/**
+ * Created by Makoy on 10/28/16.
+ */
+public class ServerAddress {
+
+    public static final String HOST_NAME = "api.bookshare.org";
+}
