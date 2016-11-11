2f87a7077c84afb2f1bce0ed77f4d1ae284e6bf3
Marco Sanchez <marco.avantica@avantica.net>
2016-11-11 12:03:09 -0600

    new class to contains creation of the action bar menu

---
+++ FBReader/src/main/java/org/geometerplus/zlibrary/ui/android/library/ZLAndroidActivityforActionBar.java
@@ -0,0 +1,12 @@
+package org.geometerplus.zlibrary.ui.android.library;
+
+
+
+ß
+/**
+ * Created by Makoy on 11/11/16.
+ */
+
+public abstract class ZLAndroidActivityforActionBar extends ZLAndroidActivity {
+
+}
