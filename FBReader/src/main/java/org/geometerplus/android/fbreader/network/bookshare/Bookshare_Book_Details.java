328af019dd683b54d39ff12fce97499b9580f7a5
Miguel Villalobos <miguel.villalobos@avantica.net>
2016-12-15 17:01:54 -0600

    According to mockup button shouldn't show if user is OM.

--- FBReader/src/main/java/org/geometerplus/android/fbreader/network/bookshare/Bookshare_Book_Details.java
+++ FBReader/src/main/java/org/geometerplus/android/fbreader/network/bookshare/Bookshare_Book_Details.java
@@ -339,6 +339,9 @@ public class Bookshare_Book_Details extends Activity implements OnClickListener 
                 Log.d("checking images", String.valueOf(imagesAvailable));
                 btnDownloadWithImages.setVisibility(View.GONE);
             }
+            if(isOM){
+                btnReadingList.setVisibility(View.GONE);
+            }
             // Set the fields of the layout with book details
             if (metadata_bean.getTitle() != null) {
                 for (int i = 0; i < metadata_bean.getTitle().length; i++) {
